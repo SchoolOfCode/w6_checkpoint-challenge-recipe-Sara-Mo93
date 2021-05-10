ce3941ad7c2acc0d78761b330fac702e168376ef
Sara Mohamoud <sara_mohamoud@outlook.com>
2021-05-10 20:08:08 +0100

    display some data

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
+const APP_ID = "f9a8cc79";
+const APP_KEY = "ce118f0a4e97bdffc78a0ab134eec3ea";
+let calorieInformation= document.getElementById("info")
+let linkButton= document.getElementById("view-button")
+let recipeName= document.getElementById("recipe-label")
+let recipeImage= document.getElementById("image")
+let foodToSearch = null;
 
-const APP_ID = "f9a8cc79";
-const APP_KEY = "ce118f0a4e97bdffc78a0ab134eec3ea";
-
-let foodToSearch = null;
-
-
-
-function handleRecipeClick() {
-  fetchRecipe(foodToSearch);
+function handleRecipeClick() {
+fetchRecipe(foodToSearch);
 }
-function handleFoodChange() {
-  foodToSearch = document.querySelector("#food-input").value;
+
+function handleFoodChange() {
+foodToSearch = document.querySelector("#food-input").value;
 }
-async function fetchRecipe(food) {
-const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=6`;
-const response = await fetch (requestUrl);
-const data = await response.json();
-const recipe = data.hits[0];
-let element = document.getElementById("recipe-label");
-element.innerHTML = recipe.recipe.label;
-element.href = recipe.recipe.url;
+
+async function fetchRecipe(food) {
+const requestUrl = `https://api.edamam.com/search?q=${food}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=6`;
+const response = await fetch (requestUrl);
+const data = await response.json();
+const recipes = data.hits[0, 1, 2, 3, 4, 5];
+recipeName.innerHTML = foodToSearch
+calorieInformation.innerHTML += recipes.recipe.calories;
+recipeName.innerText= recipes.recipe.label;
+linkButton.href= recipes.recipe.url;gi
+recipeImage.innerText= recipes.recipe.healthLabels.image;
 console.log(data.hits);
+
+
+// food.map((recipes)=> { foodToSearch += recipes.recipe.label;
+
+// });
 }
 
 
